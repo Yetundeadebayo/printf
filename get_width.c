bab157a89d5e7562ba3fde8b313eb9a1c1d05f21
Yetundeadebayo <yetunde.sheu123@gmail.com>
2023-10-15 13:02:20 -0700

    Updated _printf.c, get_width.c, handle_print.c, and main.c

--- get_width.c
+++ get_width.c
@@ -3,7 +3,7 @@
 /**
  * get_width - Calculates the width for printing
  * by Yetunde Adebayo 
-* @format: Formatted string in which to print the arguments.
+ * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
@@ -31,7 +31,7 @@ int get_width(const char *format, int *i, va_list list)
 			break;
 	}
 
-	*i = curr_i - 1;
+8	*i = curr_i - 1;
 
 	return (width);
 }
